8dfee530f75d4d1c6b4db633838070dcc6ab9fb9
mohamedali382 <ma392213@gmail.com>
2025-04-28 01:38:43 +0300

    last updates

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,29 @@
+const UserService = require('../services/UserService');
+
+exports.register = async (req,res) =>{
+try{
+const {user, token} = await UserService.register(req.body);
+res.status(201).json({
+    message: 'User registered successfully',
+    user,
+    token
+});
+}
+catch (error){
+    res.status(400).json({ error: error.message });
+}
+};
+
+exports.login = async (req, res) => {
+    try{
+        const {user, token} = UserService.login(req.body);
+        res.status(200).json({
+            message: 'user login successfully',
+            user,
+            token
+        });
+    }
+    catch (error){
+        res.status(400).json({ error: error.message });
+    }
+}
---
+++ backend/src/services/UserService.js
@@ -0,0 +1,60 @@
+const User = require('../models/User.model');
+
+const jwt = require('jsonwebtoken');
+
+
+// Normally keep this in env variables
+const JWT_SECRET = "yourSuperSecretKey";
+// fel evn ely oltly 3aleha el sobh 
+
+
+class UserService{
+    async register({name, username, email, phoneNumber,password})
+    {
+        const existingUser = await User.findOne({email});
+        if (existingUser)
+        {
+            throw new Error('User is already exists');
+        }
+
+        const newUser = new User({
+            name,
+            username,
+            email,
+            phoneNumber,
+            password
+        });
+
+        await newUser.save();
+
+        const token = this.generateToken(newUser._id);
+        return { user: newUser, token };
+    }
+
+    async login({username,password})
+    {
+        const user = await User.findOne({username});
+
+        if(!user)
+        {
+            throw new Error('Invalid username or password');
+        }
+        const isMatch = await bycrpt.compare(password, user.password);
+        if(!isMatch)
+        {
+            throw new Error('Invalid username or password');
+        }
+        const token = this.generateToken(user._id);
+
+        return { user, token };
+    
+    }
+
+    generateToken(userId)
+    {
+        return jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: '7d' });
+    }
+
+}
+
+module.exports = new UserService();
