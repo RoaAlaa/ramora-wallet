ea36f203b111415634e7429c7b68767ede79aebc
Amira <amiraashraf352@gmail.com>
2025-04-29 02:02:51 +0300

    nav-bar to be used in all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import HomePage from './pages/HomePage';
 import Footer from './components/Common/Footer';
-import SignUpPage from './components/User/Register'
-import LoginPage from './pages/LoginPage'
-import SendMoneyPage from './pages/SendMoneyPage'
+import SignUpPage from './components/User/Register';
+import LoginPage from './pages/LoginPage';
+import SendMoneyPage from './pages/SendMoneyPage';
+import Navbartwo from './components/Common/Navbartwo';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
 
       {/* <SendMoneyPage /> */}
-      <Footer />
+      {/* <Footer /> */}
       
       {/* <SignUpPage/> */}
+      <Navbartwo />
       
     </div>
   );
