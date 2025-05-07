a48c4ad55ab4c9baaa20f4e9b6688e75eb609d0f
Roa Alaa <roaelsayed@hotmail.com>
2025-05-08 01:03:31 +0300

    backend rg3to zy mkan

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 require('dotenv').config(); // Ensure environment variables are loaded
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,27 +1,16 @@
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config({ path: '../.env' }); 
 const express = require('express');
-const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5001;
-
+const PORT = process.env.PORT || 5001; 
 app.use(express.json());
-
 const userRoutes = require('./routes/userRoute');
 
-// Connect to MongoDB using the URI from .env
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('MongoDB connected successfully');
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
+// Connect to MongoDB
+connectDB();
 
 app.use('/api/users', userRoutes);
-
 app.get('/', (req, res) => {
   res.send('Hello from the Backend!');
 });
